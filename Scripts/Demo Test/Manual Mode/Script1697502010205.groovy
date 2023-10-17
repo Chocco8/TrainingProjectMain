41a59c1854bb5cf8612acc121c371ba3f0dae2d2
nsantos <nsantos@PL-AU-PF11LXWT.planit.local>
2023-10-17 15:14:59 +1100

    2nd Commit

--- Scripts/Demo Test/Manual Mode/Script1697502010205.groovy
+++ Scripts/Demo Test/Manual Mode/Script1697502010205.groovy
@@ -27,7 +27,7 @@ WebUI.setEncryptedText(findTestObject('Page_OrangeHRM/input_Password_password'),
 
 WebUI.click(findTestObject('Page_OrangeHRM/button_Login'))
 
-WebUI.delay(2)
+WebUI.refresh(FailureHandling.STOP_ON_FAILURE)
 
 WebUI.closeBrowser()
 
